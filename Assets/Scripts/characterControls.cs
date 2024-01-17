c38b800f1d194be00959e38b928622856cc92631
Foestar1 <118066929+Foestar1@users.noreply.github.com>
2024-01-17 13:10:05 -0500

    Sprint added
    added sprint!!

--- Assets/Scripts/characterControls.cs
+++ Assets/Scripts/characterControls.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class characterControls : MonoBehaviour
 {
-    [Header("Movement Speed")]
+    [Header("Movement Variables")]
     [Tooltip("The players movementSpeed")]
     [SerializeField]
-    private float walkSpeed = 5.0f;
+    private float movementSpeed = 5.0f;
+    [Tooltip("Whether the player can run or not(hub related)")]
+    [SerializeField]
+    private bool canRun;
 
     [Header("Look Sensitivity")]
     [Tooltip("Mouse sensitivity for the camera")]
@@ -16,30 +19,30 @@ public class characterControls : MonoBehaviour
     private float upDownRange = 80.0f;
 
     [Header("Inputs Customization")]
-    [Tooltip("The input for horizontal character movement")]
+    [Tooltip("The input button for horizontal character movement")]
     [SerializeField]
     private string horizontalMoveInput = "Horizontal";
-    [Tooltip("The input for vertical character movement")]
+    [Tooltip("The input button for vertical character movement")]
     [SerializeField]
     private string verticalMoveInput = "Vertical";
-    [Tooltip("The input for horizontal camera control")]
+    [Tooltip("The input button for horizontal camera control")]
     [SerializeField]
     private string mouseXInput = "Mouse X";
-    [Tooltip("The input for vertical camera control")]
+    [Tooltip("The input button for vertical camera control")]
     [SerializeField]
     private string mouseYInput = "Mouse Y";
+    private float verticalRotation;
 
-    [Tooltip("The player's camera")]
+    [Header("Player References")]
+    [Tooltip("Our players view camera")]
     [SerializeField]
     private Camera playerCamera;
-
-    private float verticalRotation;
+    [Tooltip("Controls the players animations")]
+    [SerializeField]
+    private Animator playerAnimator;
+    [Tooltip("Controls the players actual movement")]
+    [SerializeField]
     private CharacterController characterController;
-
-    private void Awake()
-    {
-        characterController = GetComponent<CharacterController>();
-    }
 
     private void Update()
     {
@@ -49,8 +52,17 @@ public class characterControls : MonoBehaviour
 
     private void HandleMovement()
     {
-        float verticalSpeed = Input.GetAxis(verticalMoveInput) * walkSpeed;
-        float horizontalSpeed = Input.GetAxis(horizontalMoveInput) * walkSpeed;
+        if (canRun && Input.GetButton("Sprint"))
+        {
+            movementSpeed = 8;
+        }
+        else
+        {
+            movementSpeed = 5;
+        }
+
+        float verticalSpeed = Input.GetAxis(verticalMoveInput) * movementSpeed;
+        float horizontalSpeed = Input.GetAxis(horizontalMoveInput) * movementSpeed;
 
         Vector3 speed = new Vector3(horizontalSpeed, 0, verticalSpeed);
         speed = transform.rotation * speed;
