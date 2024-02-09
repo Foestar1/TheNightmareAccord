8067bf165e8e6d73e91a7f46dda9a7cebd0ec38f
Skully41311 <118067279+Skully41311@users.noreply.github.com>
2024-02-09 13:46:35 -0500

    graveyard light update

---
+++ Assets/Scripts/FlickerEffect.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class FlickerEffect : MonoBehaviour
+{
+    public float intensity = 0.2f;
+    public float perSecond = 0.3f;
+    public float randomness = 1.0f;
+
+    private Light _light;
+    private float _startingIntensity;
+    private float _time;
+
+    void Start()
+    {
+        _light = GetComponent<Light>(); // Get the Light component attached to this GameObject
+        _startingIntensity = _light.intensity; // Store the starting intensity
+    }
+
+    void Update()
+    {
+        _time += Time.deltaTime * (1 - Random.Range(-randomness, randomness));
+        _light.intensity = _startingIntensity + Mathf.Sin(_time * perSecond) * intensity;
+    }
+}
