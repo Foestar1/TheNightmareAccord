be514982df5621450bb11f555f6f2f1dcc003556
Foestar1 <118066929+Foestar1@users.noreply.github.com>
2024-02-29 13:57:07 -0500

    Tons of huge updates!
    Fixes, lighting baked, etc.

--- Assets/Scripts/SaveAndLoadData.cs
+++ Assets/Scripts/SaveAndLoadData.cs
@@ -7,6 +7,8 @@ public class SaveAndLoadData : MonoBehaviour
     public float soundVolume { get; set; }
     public float musicVolume { get; set; }
     public int level1Complete { get; set; }
+    public int level1CompleteSecondary { get; set; }
+    public float level1CompleteSpeed { get; set; }
     public int level2Complete { get; set; }
     public int level3Complete { get; set; }
     public int level4Complete { get; set; }
@@ -18,6 +20,8 @@ public class SaveAndLoadData : MonoBehaviour
         PlayerPrefs.SetFloat("SoundVolume", soundVolume);
         PlayerPrefs.SetFloat("MusicVolume", musicVolume);
         PlayerPrefs.SetInt("Level1Complete", level1Complete);
+        PlayerPrefs.SetInt("level1CompleteSecondary", level1CompleteSecondary);
+        PlayerPrefs.SetFloat("level1CompleteSpeed", level1CompleteSpeed);
         PlayerPrefs.SetInt("Level2Complete", level2Complete);
         PlayerPrefs.SetInt("Level3Complete", level3Complete);
         PlayerPrefs.SetInt("Level4Complete", level4Complete);
@@ -31,6 +35,8 @@ public class SaveAndLoadData : MonoBehaviour
         soundVolume = PlayerPrefs.GetFloat("SoundVolume");
         musicVolume = PlayerPrefs.GetFloat("MusicVolume");
         level1Complete = PlayerPrefs.GetInt("Level1Complete");
+        level1CompleteSecondary = PlayerPrefs.GetInt("level1CompleteSecondary");
+        level1CompleteSpeed = PlayerPrefs.GetInt("level1CompleteSpeed");
         level2Complete = PlayerPrefs.GetInt("Level2Complete");
         level3Complete = PlayerPrefs.GetInt("Level3Complete");
         level4Complete = PlayerPrefs.GetInt("Level4Complete");
@@ -48,6 +54,8 @@ public class SaveAndLoadData : MonoBehaviour
             soundVolume = 0.5f;
             musicVolume = 0.5f;
             level1Complete = 0;
+            level1CompleteSecondary = 0;
+            level1CompleteSpeed = 0;
             level2Complete = 0;
             level3Complete = 0;
             level4Complete = 0;
